cf4ee504bd175de9d81ee3383ddb20324667716c
Gastón Otero <48187361+GastonOtero@users.noreply.github.com>
2022-10-12 12:32:11 -0300

    Create Remove String Spaces.js

---
+++ Remove String Spaces.js
@@ -0,0 +1,11 @@
+/*
+DESCRIPTION:
+
+Simple, remove the spaces from the string, then return the resultant string.
+*/
+
+// My solution:
+
+function noSpace(x){
+    return x.replace(/\s/g, '');
+    }
